bc6f9a79c22ddff74b521e235a4cb00e1dd55000
iblethebible <iblethebible@zoho.com>
2022-06-19 22:01:22 +0100

    Create getjob.php

---
+++ getjob.php
@@ -0,0 +1,45 @@
+<?php
+include('database_connect_windaninjaz.php');
+
+
+?>
+<html>
+    <head><title>View all jobs</title></head>
+    <body>
+        <h1>All jobs</h1>
+        <a href="addjob.php">Add a job</a>
+<?php
+$sql = "SELECT customer.custName, job.cust_id, job.houseNumName, job.streetName,job.price, job.dateLastDone
+FROM customer, job
+WHERE job.cust_id = customer.id";
+
+
+echo "<p>The SQL is: " . $sql . "</p>";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	echo '<table border="1">';
+	echo '<tr>';
+	echo '<th>Name</th>';
+	echo '<th>Price</th>';
+	
+	echo '<th>UPDATE</th>';
+	echo '<th>DELETE</th>';
+	echo '</tr>';
+}
+    while ($row=$result->fetch_assoc()) {
+
+        echo '<tr>';
+        echo '<td>' . $row["custName"] . '</td>';
+        echo '<td>' . $row["houseNumName"] . '</td>';
+        echo '<td>' . $row["streetName"] . '</td>';
+        echo '</table>';
+    
+        
+        
+
+    }
+
+?>
+</table>
+</html>
