22231d7200b272c000bdf7444f9e9336ad13027b
iblethebible <iblethebible@zoho.com>
2022-06-21 00:37:21 +0100

    Update database_connect_windaninjaz.php

--- database_connect_windaninjaz.php
+++ database_connect_windaninjaz.php
@@ -1,5 +1,4 @@
 <?php
-echo "<p>connecting to database</p>";
 $servername = "localhost";
 $username = "user1";
 $password = "windaninja";
@@ -10,5 +9,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("connection Failed: " . $conn->connect_error);
 }
-echo "<p>Connected to database</p>";
+
 ?>
