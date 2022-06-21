4f3e336a5f6088cdca1bf34325279659213799cd
iblethebible <iblethebible@zoho.com>
2022-06-21 19:08:11 +0100

    Update jobcomplete.php
    Finally got the code to update the record of the specific job.
    Also managed to return to previous page with "header".
    Issue: When i return to previous page it makes me put in the zone again, annoying??? Will need to get variable for zone id then concatenate it into the header url

--- jobcomplete.php
+++ jobcomplete.php
@@ -1,21 +1,24 @@
 <?php
 include('database_connect_windaninjaz.php');
+$id = $_GET['job'];
+echo $id;
+
 $sql = "UPDATE job 
-SET dateLastDone = (NOW();
+SET dateLastDone = (NOW())
+WHERE id = $id";
+
 
 
 echo $sql . "<br>";
 if ($conn->query($sql) ===TRUE) {
     
     echo "Job Complete";
+    header("location: getjob.php");
 }
 else {
     echo "Error " . $conn->error;
 }
 
-SELECT * 
-FROM job 
-WHERE id = 
 
 
 ?>
